36222f5dfc1381bbb7299d390b277bb700f2097c
hanlin chen <hanlin487@gmail.com>
2024-05-03 12:42:08 -0700

    edit

--- 2048/2048.js
+++ 2048/2048.js
@@ -48,11 +48,6 @@ document.addEventListener('DOMContentLoaded', () =>  {
     function moveRight() {
       for (let i=0; i < 16; i++) {
         if (i % 4 === 0) {
-          // let totalOne = squares[i].innerHTML
-          // let totalTwo = squares[i+1].innerHTML
-          // let totalThree = squares[i+2].innerHTML
-          // let totalFour = squares[i+3].innerHTML
-          // let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
           let row = getRow(i)
 
           let filteredRow = row.filter(num => num)
@@ -60,10 +55,6 @@ document.addEventListener('DOMContentLoaded', () =>  {
           let zeros = Array(missing).fill(0)
           let newRow = zeros.concat(filteredRow)
   
-          // squares[i].innerHTML = newRow[0]
-          // squares[i +1].innerHTML = newRow[1]
-          // squares[i +2].innerHTML = newRow[2]
-          // squares[i +3].innerHTML = newRow[3]
           updateRow(i, newRow)
         }
       }
@@ -72,11 +63,6 @@ document.addEventListener('DOMContentLoaded', () =>  {
     function moveLeft() {
       for (let i=0; i < 16; i++) {
         if (i % 4 === 0) {
-          // let totalOne = squares[i].innerHTML
-          // let totalTwo = squares[i+1].innerHTML
-          // let totalThree = squares[i+2].innerHTML
-          // let totalFour = squares[i+3].innerHTML
-          // let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
           let row = getRow(i)
   
           let filteredRow = row.filter(num => num)
@@ -84,10 +70,6 @@ document.addEventListener('DOMContentLoaded', () =>  {
           let zeros = Array(missing).fill(0)
           let newRow = filteredRow.concat(zeros)
   
-          // squares[i].innerHTML = newRow[0]
-          // squares[i +1].innerHTML = newRow[1]
-          // squares[i +2].innerHTML = newRow[2]
-          // squares[i +3].innerHTML = newRow[3]
           updateRow(i, newRow)
         }
       }
@@ -113,11 +95,6 @@ document.addEventListener('DOMContentLoaded', () =>  {
   
     function moveUp() {
       for (let i=0; i < 4; i++) {
-        // let totalOne = squares[i].innerHTML
-        // let totalTwo = squares[i+width].innerHTML
-        // let totalThree = squares[i+(width*2)].innerHTML
-        // let totalFour = squares[i+(width*3)].innerHTML
-        // let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
         let col = getCol(i)
 
         let filteredColumn = col.filter(num => num)
@@ -125,32 +102,19 @@ document.addEventListener('DOMContentLoaded', () =>  {
         let zeros = Array(missing).fill(0)
         let newColumn = filteredColumn.concat(zeros)
   
-        // squares[i].innerHTML = newColumn[0]
-        // squares[i+4].innerHTML = newColumn[1]
-        // squares[i+8].innerHTML = newColumn[2]
-        // squares[i+12].innerHTML = newColumn[3]
         updateCol(i, newColumn)
       }
     }
   
     function moveDown() {
       for (let i=0; i < 4; i++) {
-        // let totalOne = squares[i].innerHTML
-        // let totalTwo = squares[i+width].innerHTML
-        // let totalThree = squares[i+(width*2)].innerHTML
-        // let totalFour = squares[i+(width*3)].innerHTML
-        // let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
         let col = getCol(i)
   
         let filteredColumn = col.filter(num => num)
         let missing = 4 - filteredColumn.length
         let zeros = Array(missing).fill(0)
         let newColumn = zeros.concat(filteredColumn)
-  
-        // squares[i].innerHTML = newColumn[0]
-        // squares[i+4].innerHTML = newColumn[1]
-        // squares[i+8].innerHTML = newColumn[2]
-        // squares[i+12].innerHTML = newColumn[3]
+
         updateCol(i, newColumn)
       }
     }
